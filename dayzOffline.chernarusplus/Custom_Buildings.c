d537fd296e4c7d4a78b9590c280d4b594af62bc1
ficarra1002 <ficarra1002@gmail.com>
2018-09-25 23:46:43 -0500

    Added new verticallity to elektro, battery for pa
    Removed commented out appartments as the ATC is working great, no plans on going back.

--- dayzOffline.chernarusplus/Custom_Buildings.c
+++ dayzOffline.chernarusplus/Custom_Buildings.c
@@ -2,20 +2,6 @@
 
 static void AddBuildings()
 {
-	/*
-	Appartment spawn - Replaced by ATC
-	
-	ItemBase m_apt;
-    vector apt_pos;
-    vector apt_dir;
-   
-    apt_pos[0] = 5239.7;
-    apt_pos[1] = 17.5;
-    apt_pos[2] = 2184.65;
-
-	m_apt = g_Game.CreateObject("Land_Tenement_Small", apt_pos, false); 
-	
-	*/	
 
     //ATC Spawn, replaces appartment
 	
@@ -35,8 +21,7 @@ static void AddBuildings()
 	
 	m_atc = g_Game.CreateObject("Land_Mil_ATC_Big", atc_pos, false);
 	m_atc.SetOrientation(atc_dir);
-	
-	
+		
 	
 	//Crashed C130
 		
@@ -72,4 +57,95 @@ static void AddBuildings()
 
 	m_scf1 = g_Game.CreateObject("Land_Misc_Scaffolding", scf1_pos, false);
 	m_scf1.SetOrientation(scf1_dir);
+	
+	//Stairs to the roofs of housing blocks in elektro
+	
+	ItemBase m_scf2;
+    vector scf2_pos;
+    vector scf2_dir;
+   
+    scf2_pos[0] = 10396.3;
+    scf2_pos[1] = 6.9456;
+    scf2_pos[2] = 2305.37;
+ 
+    scf2_dir[0] = -39.000015;
+    scf2_dir[1] = 0;
+    scf2_dir[2] = 0;
+
+	m_scf2 = g_Game.CreateObject("Land_Castle_Stairs", scf2_pos, false);
+	m_scf2.SetOrientation(scf2_dir);
+	
+	
+	//Ramp from lower roof to roof you can't easily jump to
+	
+	
+	ItemBase m_rmp1;
+    vector rmp1_pos;
+    vector rmp1_dir;
+   
+    rmp1_pos[0] = 10397.7;
+    rmp1_pos[1] = 16.0457;
+    rmp1_pos[2] = 2315.91;
+ 
+    rmp1_dir[0] = -33;
+    rmp1_dir[1] = 0;
+    rmp1_dir[2] = -31;
+
+	m_rmp1 = g_Game.CreateObject("Land_BusStop_City", rmp1_pos, false);
+	m_rmp1.SetOrientation(rmp1_dir);
+	
+	
+	//New building acting as bridge. Doesn't look right, WIP. 
+	
+	ItemBase m_bld1;
+    vector bld1_pos;
+    vector bld1_dir;
+   
+    bld1_pos[0] = 10445.2;
+    bld1_pos[1] = 13.5101;
+    bld1_pos[2] = 2299.81;
+ 
+    bld1_dir[0] = -35;
+    bld1_dir[1] = 0;
+    bld1_dir[2] = 0;  //Possibly -3.46, but I think that was just user error on my logging. If it's ugly, change. 
+
+	m_bld1 = g_Game.CreateObject("Land_HouseBlock_2F6", bld1_pos, false);
+	m_bld1.SetOrientation(bld1_dir);
+	
+	
+	//Catwalk made of boat to bridge two buildings. To be replaced with better looking model
+	
+	ItemBase m_cat1;
+    vector cat1_pos;
+    vector cat1_dir;
+   
+    cat1_pos[0] = 10451.9;
+    cat1_pos[1] = 17.739;
+    cat1_pos[2] = 2340.81;
+ 
+    cat1_dir[0] = 34;
+    cat1_dir[1] = 3;
+    cat1_dir[2] = -25;
+
+	m_cat1 = g_Game.CreateObject("Land_Boat_Small3", cat1_pos, false);
+	m_cat1.SetOrientation(cat1_dir);
+	
+	
+	//Battery for Elektro PA system. 
+	
+	ItemBase m_bat1;
+    vector bat1_pos;
+    vector bat1_dir;
+   
+    bat1_pos[0] = 10210.2;
+    bat1_pos[1] = 25.5689;
+    bat1_pos[2] = 2288;
+ 
+    bat1_dir[0] = 140.72;
+    bat1_dir[1] = -1;
+    bat1_dir[2] = -.43;
+
+	m_bat1 = g_Game.CreateObject("CarBattery", bat1_pos, false);
+	m_bat1.SetOrientation(bat1_dir);
+	
 }
