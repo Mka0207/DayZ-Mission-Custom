bb1031098359732996cef4267466268a92e27484
Mka0207 <mka0207@fwkzt.com>
2018-09-27 09:46:20 -0400

    Loot Cleanup Count support.

--- mods/ZombieMod/Addons/ZombieMod/scripts/4_World/Custom_LootCleanUp.c
+++ mods/ZombieMod/Addons/ZombieMod/scripts/4_World/Custom_LootCleanUp.c
@@ -7,6 +7,7 @@ ref TStringArray Loadout_List = {
 "BalaclavaMask_Blackskull",
 "GorkaHelmet_Black",
 "CargoPants_Black",
+"AssaultBag_Black",
 
 "Armband_Black",
 "Armband_Blue",
@@ -64,7 +65,7 @@ ref TStringArray Loadout_List = {
 //Look into using a timer for this system in the future.
 
 //How many ticks before loot should be cleaned up.
-protected int LOOT_CLEANUP_SECS = 15;
+const float LOOT_CLEANUP_SECS = 120;
 
 //default float, dont edit this.
 protected float	m_LootCheckTimer = 0.0;
@@ -89,12 +90,32 @@ void OnPlayerLootTick(PlayerBase player, float curTime)
 		//Check the players radius including cargos.
 		GetGame().GetObjectsAtPosition( player.GetPosition(), m_LootMeterRadius, objects, proxyCargos );
 		
+		//How many items were cleaned.
+		int cleaned_items = 0;
+		int total_cleaned = 0;
+		
+		int cleaned_ragdolls = 0;
+		int total_cleanedrags = 0;
+		
 		if ( GetGame() && objects )
 		{
 			if ( objects.Count() >= 1 )
 			{
 				for ( int i = 0; i < objects.Count(); i++ )
 				{
+					//Handle Bodies
+					if ( objects.Get(i).ClassName() == "SurvivorBase" ) //Class.CastTo( ent_man, objects.Get(i) )
+					{
+						Man ent_man = objects.Get(i);
+						if ( !ent_man.IsAlive() ) 
+						{
+							//Print( "[DEBUG] - Class Name "+objects.Get(i).ClassName() )
+							cleaned_ragdolls = cleaned_ragdolls + 1;
+							ent_man.Delete();
+						}
+					}
+
+					//Handle Loot
 					Object item = objects.Get( i );
 					if ( item )
 					{
@@ -104,31 +125,27 @@ void OnPlayerLootTick(PlayerBase player, float curTime)
 							{
 								Print("[DEBUG] - Removed Item "+item.GetType() + " idx = " + i.ToString());
 								//Print("[DEBUG] - Removed Item "+item.ClassName() + " idx = " + i.ToString());
-							
-								//Announce that we cleaned the world.
-								GetGame().ChatPlayer( 0, "[Items_Cleaned] = "+objects.Count() );
+								
+								total_cleaned = total_cleaned + 1;
 								
 								//Delete the items from the world and the table.
 								GetGame().ObjectDelete(item);
 								objects.Remove(i);
 							}
-							
-							//Remove dead players that disconnected or that are AFK.
-							if ( !player.IsAlive() )
-							{
-								// remove the body
-								Print("[DEBUG] - Removed Player Body");
-								player.Delete();	
-							}
 						}
 					}
 				}
 			}
+			
+			//Announce that we cleaned the world.
+			GetGame().ChatPlayer( 0, "[Items_Cleaned] = "+total_cleaned );
+			GetGame().ChatPlayer( 0, "[Bodies_Cleaned] = "+cleaned_ragdolls );
 		}
 		else
 		{
 			//Check if there is a table.
 			Print("[DEBUG] - NO TABLE!");
 		}
+
 	}
 }
