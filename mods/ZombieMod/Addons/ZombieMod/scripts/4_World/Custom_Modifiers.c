ae016114d0e22798eb4e721279be8f2af819283f
Mka0207 <mka0207@fwkzt.com>
2018-09-27 01:30:53 -0400

    Modifiers Update.

---
+++ mods/ZombieMod/Addons/ZombieMod/scripts/4_World/Custom_Modifiers.c
@@ -0,0 +1,25 @@
+//Disabled as they arent needed yet in death-match.
+
+modded class Hunger: ModifierBase
+{	
+	override void OnTick(PlayerBase player, float deltaT)
+	{
+		//Print( "[DEBUG] : Hunger is disabled" )
+	}
+}
+
+modded class Thirst: ModifierBase
+{	
+	override void OnTick(PlayerBase player, float deltaT)
+	{
+		//Print( "[DEBUG] : Thirst is disabled" )
+	}
+}
+
+modded class Temperature: ModifierBase
+{	
+	override void OnTick(PlayerBase player, float deltaT)
+	{
+		//Print( "[DEBUG] : Temperature is disabled" )
+	}
+}
