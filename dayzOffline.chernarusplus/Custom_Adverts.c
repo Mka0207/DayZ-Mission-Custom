8ac223884581f9ea428e4ea8de0f460ae449e3af
ficarra1002 <ficarra1002@gmail.com>
2018-10-10 13:44:58 -0500

    New adverts

--- dayzOffline.chernarusplus/Custom_Adverts.c
+++ dayzOffline.chernarusplus/Custom_Adverts.c
@@ -1,6 +1,6 @@
 //Advert System by mka0207@fwkzt.com
 
-const float ADVERT_DELAY = 360;
+const float ADVERT_DELAY = 120;
 float AdCheckTimer = 0.0;
 
 TStringArray Timed_Adverts()
@@ -9,13 +9,13 @@ TStringArray Timed_Adverts()
 	"Contact Us @ discord.gg/npTNKeM",
 	"Support Us @ fwkzt.com/donate",
 	"Melee weapons KO on first hit!",
-	"Bodies auto clean after 18 secs!",
-	"Items auto clean after 3 mins!",
+	"Bodies and loot are automatically removed on a timer!",
 	"Maintained and developed by FWKZT.com",
 	"Want to team up? Use Armbands!",
-	"Occasional lag spikes are normal.",
 	"Server Changelog posted on discord.",
+	"If you ever suspect a hacker, don't hesitate to type 'Admin' in chat, we'll look into it.",
 	"Try NOT to spawn camp!",
+	"We are launching a survival server soon! Check our discord for info: discord.gg/npTNKeM",
 	"Server restarts every 4-6 hours!"};
 }
 
